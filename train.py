b8093b1bd4b220b38d7ed37a5423815d06871a0c
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-10-25 22:31:52 +0500

    update code

--- train.py
+++ train.py
@@ -315,7 +315,7 @@ def train_sam(
 
    
 
-            loss_total =  loss_focal+ loss_dice + 0.1*loss_dist #+ loss_iou  + loss_focal +
+            loss_total =  0.4*loss_focal+ 0.1*loss_dice + 0.1*loss_dist #+ loss_iou  + loss_focal +
 
             fabric.backward(loss_total)
 
