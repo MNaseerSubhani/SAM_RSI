02d1a7218a78bc75feb6517c3ce59efeb9fa076f
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-10-27 08:47:50 +0500

    update code

--- train.py
+++ train.py
@@ -202,6 +202,8 @@ def train_sam(
             del data
             prompts = get_prompts(cfg, bboxes, gt_masks)
 
+            batch_size = img_tensor.size(0)
+
             entropy_maps, preds = process_forward(images_weak, prompts, model)
             pred_stack = torch.stack(preds, dim=0)
             pred_binary = (pred_stack > 0.99).float() 
