9f009ae7d9d8ddb0ba791e69c2d6aaae2dc9c063
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-11-02 11:17:41 +0500

    update code

--- train_resam.py
+++ train_resam.py
@@ -319,7 +319,7 @@ def train_sam(
                     pred_w_overlap = (entropy_map) * invert_overlap_map[0]
 
 
-                    ys, xs = torch.where(pred_w_overlap[0] > 0.5)
+                    ys, xs = torch.where(pred_w_overlap > 0.5)
                     if len(xs) > 0 and len(ys) > 0:
                         x_min, x_max = xs.min().item(), xs.max().item()
                         y_min, y_max = ys.min().item(), ys.max().item()
