da014a12cb9d73e2096f441baac038cf306d8151
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-11-05 16:19:24 +0500

    update code

--- train_resam.py
+++ train_resam.py
@@ -319,7 +319,7 @@ def train_sam(
             
                 del  pred_masks, iou_predictions 
             
-                loss_total =  20 * loss_focal +  loss_dice  + loss_iou + 0.1*loss_sim #+ loss_iou  +  +
+                loss_total =   loss_focal #+  loss_dice  + loss_iou + 0.1*loss_sim #+ loss_iou  +  +
 
                 
 
