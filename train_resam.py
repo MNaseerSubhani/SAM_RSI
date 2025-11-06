2dc611060a9b0762c5c8a5e1815b2bf8bccd2dd7
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-11-06 21:49:10 +0500

    update code

--- train_resam.py
+++ train_resam.py
@@ -621,7 +621,7 @@ def train_sam(
                 torch.cuda.empty_cache()
 
 
-                loss_total =  20 * loss_focal +  loss_dice  + loss_iou + loss_sim#+ loss_iou  +  +
+                loss_total =  20 * loss_focal +  loss_dice  + loss_iou #+ loss_sim#+ loss_iou  +  +
 
 
 
