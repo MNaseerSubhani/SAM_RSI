ca6667d942692e08560f8ac162e004ecdc70e6a0
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-11-06 11:27:22 +0500

    update code

--- train_resam.py
+++ train_resam.py
@@ -319,10 +319,10 @@ def train_sam(
                         soft_mask = (soft_mask > 0).float()
               
                        
-                        plt.imshow(pred_mask[2].detach().cpu().numpy(), cmap='viridis')
-                        plt.show()
-                        plt.imshow(soft_mask[2].detach().cpu().numpy(), cmap='viridis')
-                        plt.show()
+                        # plt.imshow(pred_mask[2].detach().cpu().numpy(), cmap='viridis')
+                        # plt.show()
+                        # plt.imshow(soft_mask[2].detach().cpu().numpy(), cmap='viridis')
+                        # plt.show()
 
                        
 
