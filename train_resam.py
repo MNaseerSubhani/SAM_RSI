34d570bd8f555923aff560931f2df619a3119309
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-11-08 00:10:34 +0500

    update code

--- train_resam.py
+++ train_resam.py
@@ -1845,7 +1845,8 @@ def train_sam(
 
                 entropy_maps, preds = process_forward(images_weak, prompts, model)
                 pred_stack = torch.stack(preds, dim=0)
-                pred_binary = (pred_stack > 0.9).float() 
+                entropy_maps = torch.stack(entropy_maps, dim=0)
+                pred_binary = (((pred_stack ) * (1-entropy_maps))>0.5).float() 
                 overlap_count = pred_binary.sum(dim=0)
                 overlap_map = (overlap_count > 1).float()
                 invert_overlap_map = 1.0 - overlap_map
@@ -1856,8 +1857,8 @@ def train_sam(
                 for i, (entr_map, pred) in enumerate(zip(entropy_maps, preds)):
                     entr_norm = (entr_map - entr_map.min()) / (entr_map.max() - entr_map.min() + 1e-8)
                     entr_vis = (entr_norm[0].cpu().numpy() * 255).astype(np.uint8)
-                    pred = (pred[0]>0.9)
-                    pred_w_overlap = pred * invert_overlap_map[0]
+                    # pred = (pred[0]>0.9)
+                    pred_w_overlap = pred[0] * invert_overlap_map[0]
 
                     ys, xs = torch.where(pred_w_overlap > 0.5)
                     if len(xs) > 0 and len(ys) > 0:
