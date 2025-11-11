2ce43851806ef0c724b7a07a8b02b160d29a5f39
MNaseersubhani <muhammadnaseersubhani@gmail.com>
2025-11-11 21:52:29 +0500

    update

--- scripts/train_nwpu_pointsam.sh
+++ scripts/train_nwpu_pointsam.sh
@@ -9,6 +9,6 @@ output_dirs=("work_dir/nwpu/pointsam")
 for output_dir in "${output_dirs[@]}"; do
     for num_points in "${num_points_list[@]}"; do
         out_dir="${output_dir}/point_${num_points}"
-        CUDA_VISIBLE_DEVICES=0 python3.10 train_pointsam.py --cfg "$cfg_file" --prompt "$prompt" --num_points "$num_points" --out_dir "$out_dir" --load_type "$load_type"
+        CUDA_VISIBLE_DEVICES=0 MPLBACKEND=Agg python3.10 train_pointsam.py --cfg "$cfg_file" --prompt "$prompt" --num_points "$num_points" --out_dir "$out_dir" --load_type "$load_type"
     done
 done
